4c9c531d5c9be405d6f3737379a9da051517a106
Vlad <44514204+vldr@users.noreply.github.com>
2023-07-22 18:49:27 -0500

    Migrate to JS style rather than using dvh.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import { render } from "solid-js/web";
 
 import "./index.css";
 
+const resize = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+resize();
+
+window.onresize = resize;
+
 const root = document.getElementById("root");
 
 render(() => <App />, root!);
